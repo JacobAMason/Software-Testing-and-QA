edf48ef7db6a9e0626da67d534824be83ebd9ef8
Blake <blakeaholifield@gmail.com>
2017-02-21 12:37:14 -0600

    Base dev and tests

--- Assignment1/body_mass_index.py
+++ Assignment1/body_mass_index.py
@@ -1,10 +1,80 @@
 class BodyMassIndex:
-    def __init__(self):
-        pass
+    def __init__(self, height, weight):
+        self.mass_index = 0
+        self.height = height
+        self.weight = weight
+        self.metric_height = 0
+        self.metric_weight = 0
+        self.bmi_category = ""
 
-    def hello_world(self):
-        return "hello world"
+    # convert from lbs to metric
+    def convert_weight(self, weight):
+        self.metric_weight = weight * 0.45
+        return self.metric_weight
 
+    # convert from inches to metric
+    def convert_height(self, height):
+        self.metric_height = height * 0.025
+        return self.metric_height
+
+    # calculate total BMI
+    def calculate_bmi(self, height, weight):
+        metric_height = self.convert_height(height)
+        metric_weight = self.convert_weight(weight)
+        height_squared = metric_height * metric_height
+        self.mass_index = metric_weight / height_squared
+        return self.mass_index
+
+    # determine if under, over, or normal weight
+    def determine_bmi_category(self, mass_index):
+        if mass_index <= 18.5:
+            self.bmi_category = "Underweight"
+        elif mass_index >= 25:
+            self.bmi_category = "Overweight"
+        else:
+            self.bmi_category = "Normal"
+
+        return self.bmi_category
+
+if __name__ == '__main__':
+    BMI = BodyMassIndex()
+class BodyMassIndex:
+    def __init__(self, height, weight):
+        self.mass_index = 0
+        self.height = height
+        self.weight = weight
+        self.metric_height = 0
+        self.metric_weight = 0
+        self.bmi_category = ""
+
+    # convert from lbs to metric
+    def convert_weight(self, weight):
+        self.metric_weight = weight * 0.45
+        return self.metric_weight
+
+    # convert from inches to metric
+    def convert_height(self, height):
+        self.metric_height = height * 0.025
+        return self.metric_height
+
+    # calculate total BMI
+    def calculate_bmi(self, height, weight):
+        metric_height = self.convert_height(height)
+        metric_weight = self.convert_weight(weight)
+        height_squared = metric_height * metric_height
+        self.mass_index = metric_weight / height_squared
+        return self.mass_index
+
+    # determine if under, over, or normal weight
+    def determine_bmi_category(self, mass_index):
+        if mass_index <= 18.5:
+            self.bmi_category = "Underweight"
+        elif mass_index >= 25:
+            self.bmi_category = "Overweight"
+        else:
+            self.bmi_category = "Normal"
+
+        return self.bmi_category
 
 if __name__ == '__main__':
     BMI = BodyMassIndex()
--- Assignment1/test/test_body_mass_index.py
+++ Assignment1/test/test_body_mass_index.py
@@ -1,11 +1,29 @@
 import pytest
+from pytest import approx
+
 
 
 @pytest.fixture
 def bmi():
     from Assignment1.body_mass_index import BodyMassIndex
-    return BodyMassIndex()
+    return BodyMassIndex(63, 125)
 
 
-def test_hello_world(bmi):
-    assert bmi.hello_world() == "hello world"
+def test_convert_weight(bmi):
+    assert bmi.convert_weight(bmi.weight) == 56.25
+
+
+# approx is used for floating point tolerance
+def test_convert_height(bmi):
+    assert bmi.convert_height(bmi.height) == approx(1.575)
+
+
+# approx is used to ensure floating point calculations are correct to within 2 decimal places from example in doc
+def test_bmi_calculation(bmi):
+    assert bmi.calculate_bmi(bmi.height, bmi.weight) == approx(22.7, rel=1e-2)
+
+
+def test_bmi_category(bmi):
+    # might have goofed this calcing bmi here isn't great
+    bmi.mass_index = bmi.calculate_bmi(bmi.height, bmi.weight)
+    assert bmi.determine_bmi_category(bmi.mass_index) == "Normal"
