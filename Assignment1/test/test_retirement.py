e4d19f0d488d91becf7fdbe3b77b8f6362f3a7ed
Eli Davis <eli.davis1995@gmail.com>
2017-02-24 07:11:00 -0600

    Modified and Added test for complete code coverage

--- Assignment1/test/test_retirement.py
+++ Assignment1/test/test_retirement.py
@@ -6,9 +6,13 @@ def test_actually_works():
     assert year_can_retire(35, 100000, .1, 500000) == 60
 
 
+def test_return_one_hundred_if_too_old():
+    assert year_can_retire(40, 40000, .1, 500000) == 100
+
+
 def test_should_throw_error_with_negative_age():
     with pytest.raises(ValueError):
-        year_can_retire(10, -1, 0, 0)
+        year_can_retire(-10, 1, 0, 0)
 
 
 def test_should_throw_error_with_salary_zero_or_less():
@@ -23,7 +27,7 @@ def test_should_throw_error_with_negative_savings():
 
 def test_should_throw_error_with_negative_goal():
     with pytest.raises(ValueError):
-        year_can_retire(-0, 1, 0, -1)
+        year_can_retire(0, 1, 0, -1)
 
 
 def test_should_throw_error_with_savings_larger_than_one():
