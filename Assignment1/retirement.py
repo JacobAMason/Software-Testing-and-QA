92dbca2187274924ed16823fac81fdb71f97d26a
Eli Davis <eli.davis1995@gmail.com>
2017-02-24 07:00:18 -0600

    Defined retirement function with 10 tests

--- Assignment1/retirement.py
+++ Assignment1/retirement.py
@@ -1,10 +1,38 @@
-class Retirement:
-    def __init__(self):
-        pass
+def year_can_retire(age, salary, saving, goal):
+    """
+    Input user's current age, annual salary, percentage saved (savings matched
+    by employer - so double amount saved). Input desired retirement savings
+    goal. Output what age savings goal will be met. You can assume death at
+    100 years(therefore, indicate if the savings goal is not met).
+    :param age: current age of individual
+    :param salary:
+    :param saving:
+    :param goal:
+    :return:
+    """
 
-    def hello_world(self):
-        return "hello world"
+    if isinstance(age, (int, float)) is False:
+        raise ValueError("Age must be a number!")
 
+    if isinstance(salary, (int, float)) is False:
+        raise ValueError("Salary must be a number!")
 
-if __name__ == '__main__':
-    R = Retirement()
+    if isinstance(saving, (int, float)) is False:
+        raise ValueError("Saving must be a number!")
+
+    if isinstance(goal, (int, float)) is False:
+        raise ValueError("Goal must be a number!")
+
+    if age < 0:
+        raise ValueError("Can not have a negative age")
+
+    if salary <= 0:
+        raise ValueError("Can not have a positive salary")
+
+    if saving > 1 or saving < 0:
+        raise ValueError("Saving must be bound between 0 and 1")
+
+    if goal < 0:
+        raise ValueError("Can not have a negative goal")
+
+    return min(age + (goal / (salary*saving*2)), 100)
--- Assignment1/test/test_retirement.py
+++ Assignment1/test/test_retirement.py
@@ -1,11 +1,51 @@
 import pytest
+from Assignment1.retirement import year_can_retire
 
 
-@pytest.fixture
-def r():
-    from Assignment1.retirement import Retirement
-    return Retirement()
+def test_actually_works():
+    assert year_can_retire(35, 100000, .1, 500000) == 60
 
 
-def test_hello_world(r):
-    assert r.hello_world() == "hello world"
+def test_should_throw_error_with_negative_age():
+    with pytest.raises(ValueError):
+        year_can_retire(10, -1, 0, 0)
+
+
+def test_should_throw_error_with_salary_zero_or_less():
+    with pytest.raises(ValueError):
+        year_can_retire(10, 0, 0, 0)
+
+
+def test_should_throw_error_with_negative_savings():
+    with pytest.raises(ValueError):
+        year_can_retire(10, 1, -1, 0)
+
+
+def test_should_throw_error_with_negative_goal():
+    with pytest.raises(ValueError):
+        year_can_retire(-0, 1, 0, -1)
+
+
+def test_should_throw_error_with_savings_larger_than_one():
+    with pytest.raises(ValueError):
+        year_can_retire(0, 1, 2, 1)
+
+
+def test_should_raise_error_when_age_is_not_a_number():
+    with pytest.raises(ValueError):
+        year_can_retire("0", 1, 1, 1)
+
+
+def test_should_raise_error_when_salary_is_not_a_number():
+    with pytest.raises(ValueError):
+        year_can_retire(0, "1", 1, 1)
+
+
+def test_should_raise_error_when_saving_is_not_a_number():
+    with pytest.raises(ValueError):
+        year_can_retire(0, 1, "1", 1)
+
+
+def test_should_raise_error_when_goal_is_not_a_number():
+    with pytest.raises(ValueError):
+        year_can_retire(0, 1, 1, "1")
