c4c868a5ad894bcf246319d64b22e08b70317fa3
RickArendall <rickarendall@gmail.com>
2017-02-23 23:41:14 -0600

    Added baseline (0) distance and its test

--- Assignment1/distance_formula.py
+++ Assignment1/distance_formula.py
@@ -1,9 +1,16 @@
 class DistanceFormula:
-    def __init__(self):
-        pass
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.distance = 0
 
     def hello_world(self):
         return "hello world"
+
+    def compute_distance(self):
+        return self.distance
 
 
 if __name__ == '__main__':
--- Assignment1/test/test_distance_formula.py
+++ Assignment1/test/test_distance_formula.py
@@ -4,8 +4,12 @@ import pytest
 @pytest.fixture
 def df():
     from Assignment1.distance_formula import DistanceFormula
-    return DistanceFormula()
+    return DistanceFormula(0, 0, 0, 0)
 
 
 def test_hello_world(df):
     assert df.hello_world() == "hello world"
+
+
+def test_distance_is_0(df):
+    assert df.compute_distance() == 0
