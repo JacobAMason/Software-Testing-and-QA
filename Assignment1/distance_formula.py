413d49b4cd0271b4e416a804ec5fc3dc5bde8e8f
RickArendall <rickarendall@gmail.com>
2017-02-24 00:22:21 -0600

    Added extra tests to cover distance formula. Closes #4

--- Assignment1/distance_formula.py
+++ Assignment1/distance_formula.py
@@ -1,5 +1,6 @@
 from math import sqrt
 
+
 class DistanceFormula:
     def __init__(self, x1, y1, x2, y2):
         self.x1 = x1
--- Assignment1/test/test_distance_formula.py
+++ Assignment1/test/test_distance_formula.py
@@ -22,9 +22,81 @@ def test_distance_is_1_x_axis_only(df):
     assert df.compute_distance() == approx(1)
 
 
+def test_distance_is_1_x_axis_only_neg_val(df):
+    df.x1 = -2
+    df.x2 = -1
+    assert df.compute_distance() == approx(1)
+
+
+def test_distance_is_4_x_axis_only_pos_neg(df):
+    df.x1 = 2
+    df.x2 = -2
+    assert df.compute_distance() == approx(4)
+
+
+def test_distance_is_1_x_axis_only_0_to_1(df):
+    df.x1 = 0
+    df.x2 = 1
+    assert df.compute_distance() == approx(1)
+
+
+def test_distance_is_1_x_axis_only_1_to_0(df):
+    df.x1 = 1
+    df.x2 = 0
+    assert df.compute_distance() == approx(1)
+
+
+def test_distance_is_1_x_axis_only_0_to_neg_1(df):
+    df.x1 = 0
+    df.x2 = -1
+    assert df.compute_distance() == approx(1)
+
+
+def test_distance_is_1_x_axis_only_neg_1_to_0(df):
+    df.x1 = -1
+    df.x2 = 0
+    assert df.compute_distance() == approx(1)
+
+
 def test_distance_is_1_y_axis_only(df):
     df.y1 = 2
     df.y2 = 1
+    assert df.compute_distance() == approx(1)
+
+
+def test_distance_is_1_y_axis_only_neg_val(df):
+    df.y1 = -2
+    df.y2 = -1
+    assert df.compute_distance() == approx(1)
+
+
+def test_distance_is_4_y_axis_only_pos_neg(df):
+    df.y1 = 2
+    df.y2 = -2
+    assert df.compute_distance() == approx(4)
+
+
+def test_distance_is_1_y_axis_only_0_to_1(df):
+    df.y1 = 0
+    df.y2 = 1
+    assert df.compute_distance() == approx(1)
+
+
+def test_distance_is_1_y_axis_only_1_to_0(df):
+    df.y1 = 1
+    df.y2 = 0
+    assert df.compute_distance() == approx(1)
+
+
+def test_distance_is_1_y_axis_only_0_to_neg_1(df):
+    df.y1 = 0
+    df.y2 = -1
+    assert df.compute_distance() == approx(1)
+
+
+def test_distance_is_1_y_axis_only_neg_1_to_0(df):
+    df.y1 = -1
+    df.y2 = 0
     assert df.compute_distance() == approx(1)
 
 
@@ -35,3 +107,38 @@ def test_distance_is_1_each_axis(df):
     df.x2 = 1
     # Difference of 1 on each axis should produce square root(2)overall
     assert df.compute_distance() == approx(1.414213562)
+
+
+def test_distance_is_1_each_axis_neg_val(df):
+    df.y1 = -2
+    df.y2 = -1
+    df.x1 = -2
+    df.x2 = -1
+    # Difference of 1 on each axis should produce square root(2)overall
+    assert df.compute_distance() == approx(1.414213562)
+
+
+def test_distance_both_axes_extreme_x(df):
+    df.y1 = 0
+    df.y2 = 10
+    df.x1 = 2
+    df.x2 = 1000000000
+    # Extreme difference in x values should override the difference in y
+    assert df.compute_distance() == approx(1000000000)
+
+
+def test_distance_both_axes_extreme_y(df):
+    df.y1 = 0
+    df.y2 = 1000000000
+    df.x1 = 0
+    df.x2 = 20
+    # Extreme difference in y values should override the difference in x
+    assert df.compute_distance() == approx(1000000000)
+
+
+def test_distance_both_axes_shorter_than_1(df):
+    df.y1 = 0
+    df.y2 = 0.5
+    df.x1 = 0
+    df.x2 = 0.3
+    assert df.compute_distance() == approx(0.5830951895)
